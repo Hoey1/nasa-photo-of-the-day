f7bf089466b3272d28ebb02866334d24010a24e6
Joey <joey@xm8.ninja>
2020-05-13 21:14:43 -0400

    completely forgot about adding axios library

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 import { BASE_URL, API_KEY } from "./constants";
